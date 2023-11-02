3a1bb013a922416d8413ffa204205f220a61db02
itschip <chip@projecterror.dev>
2023-11-02 21:22:19 +0100

    add flag support

--- cmd/create.go
+++ cmd/create.go
@@ -29,12 +29,16 @@ const logo = `
 `
 
 var (
-	logoStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("#01FAC6")).Bold(true)
-	endingMsgStyle = lipgloss.NewStyle().PaddingLeft(1).Foreground(lipgloss.Color("170")).Bold(true)
+	logoStyle           = lipgloss.NewStyle().Foreground(lipgloss.Color("#01FAC6")).Bold(true)
+	endingMsgStyle      = lipgloss.NewStyle().PaddingLeft(1).Foreground(lipgloss.Color("170")).Bold(true)
+	allowedProjectTypes = []string{"chi", "gin", "fiber", "gorilla/mux", "httprouter", "standard-library"}
 )
 
 func init() {
 	rootCmd.AddCommand(createCmd)
+
+	createCmd.Flags().StringP("project-name", "n", "", "Name of project to create")
+	createCmd.Flags().StringP("project-type", "t", "", fmt.Sprintf("Type of project to create. Allowed values: %s", strings.Join(allowedProjectTypes, ", ")))
 }
 
 var createCmd = &cobra.Command{
@@ -43,38 +47,56 @@ var createCmd = &cobra.Command{
 	Long:  "Go Blueprint is a CLI tool that allows you to focus on the actual Go code, and not the project structure. Perfect for someone new to the Go language",
 
 	Run: func(cmd *cobra.Command, args []string) {
+		var tprogram *tea.Program
 
 		options := steps.Options{
 			ProjectName: &textinput.Output{},
 		}
 
+		flagName := cmd.Flag("project-name").Value.String()
+		flagType := cmd.Flag("project-type").Value.String()
+
+		isValid := isValidProjectType(flagType, allowedProjectTypes)
+		if !isValid {
+			cobra.CheckErr(fmt.Errorf("Project type '%s' is not valid. Valid types are: %s", flagType, strings.Join(allowedProjectTypes, ", ")))
+		}
+
 		project := &program.Project{
 			FrameworkMap: make(map[string]program.Framework),
+			ProjectName:  flagName,
+			ProjectType:  strings.ReplaceAll(flagType, "-", " "),
 		}
-		steps := steps.InitSteps(&options)
 
+		steps := steps.InitSteps(&options)
 		fmt.Printf("%s\n", logoStyle.Render(logo))
 
-		tprogram := tea.NewProgram(textinput.InitialTextInputModel(options.ProjectName, "What is the name of your project?", project))
-		if _, err := tprogram.Run(); err != nil {
-			log.Printf("Name of project contains an error: %v", err)
-			cobra.CheckErr(err)
-		}
-		project.ExitCLI(tprogram)
+		if project.ProjectName == "" {
+			tprogram := tea.NewProgram(textinput.InitialTextInputModel(options.ProjectName, "What is the name of your project?", project))
 
-		for _, step := range steps.Steps {
-			s := &multiInput.Selection{}
-			tprogram = tea.NewProgram(multiInput.InitialModelMulti(step.Options, s, step.Headers, project))
 			if _, err := tprogram.Run(); err != nil {
+				log.Printf("Name of project contains an error: %v", err)
 				cobra.CheckErr(err)
 			}
 			project.ExitCLI(tprogram)
 
-			*step.Field = s.Choice
+			project.ProjectName = options.ProjectName.Output
 		}
 
-		project.ProjectName = options.ProjectName.Output
-		project.ProjectType = strings.ToLower(options.ProjectType)
+		if project.ProjectType == "" {
+			for _, step := range steps.Steps {
+				s := &multiInput.Selection{}
+				tprogram = tea.NewProgram(multiInput.InitialModelMulti(step.Options, s, step.Headers, project))
+				if _, err := tprogram.Run(); err != nil {
+					cobra.CheckErr(err)
+				}
+				project.ExitCLI(tprogram)
+
+				*step.Field = s.Choice
+			}
+
+			project.ProjectType = strings.ToLower(options.ProjectType)
+		}
+
 		currentWorkingDir, err := os.Getwd()
 		if err != nil {
 			log.Printf("could not get current working directory: %v", err)
@@ -93,4 +115,13 @@ var createCmd = &cobra.Command{
 		fmt.Println(endingMsgStyle.Render("\nNext steps cd into the newly created project with:"))
 		fmt.Println(endingMsgStyle.Render(fmt.Sprintf("• cd %s\n", project.ProjectName)))
 	},
+}
+
+func isValidProjectType(input string, allowedTypes []string) bool {
+	for _, t := range allowedTypes {
+		if input == t {
+			return true
+		}
+	}
+	return false
 }
