c4cd0ac296f4a977a9a57d8c6d532c6a5dd57b3e
Nathan Helmig <nathan.helmig@itility.com>
2023-11-02 15:59:43 -0500

    FEATURE: Generate README.md on project creation

---
+++ test/cmd/api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"test/internal/server"
+)
+
+func main() {
+
+	server := server.NewServer()
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic("cannot start server")
+	}
+}
