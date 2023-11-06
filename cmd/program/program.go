32878048694f9cdc2a61eb01ce561827a19db0c9
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-11-06 15:12:13 +0530

    feat: go install air(installing globally)

--- cmd/program/program.go
+++ cmd/program/program.go
@@ -223,6 +223,13 @@ func (p *Project) CreateMainFile() error {
         return err
     }
 
+    // go install air
+    err = utils.ExecuteCmd("go", []string{"install","github.com/cosmtrek/air@latest"}, projectPath)
+	if err != nil {
+		log.Printf("Error installing air@latest: %v", err)
+		cobra.CheckErr(err)
+		return err
+	}
 	err = utils.GoFmt(projectPath)
 	if err != nil {
 		log.Printf("Could not gofmt in new project %v\n", err)
