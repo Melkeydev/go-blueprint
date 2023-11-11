8c13fee4d244b57cc1e3b5ae300600aa902fa3dd
Omar <88511754+Jamlee977@users.noreply.github.com>
2023-11-11 14:17:55 +0200

    Gives an error if a directory already exists. Colorized the errors when creating

--- cmd/create.go
+++ cmd/create.go
@@ -1,18 +1,17 @@
 package cmd
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"strings"
+        "fmt"
+        "os"
+        "strings"
 
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
-	"github.com/melkeydev/go-blueprint/cmd/program"
-	"github.com/melkeydev/go-blueprint/cmd/steps"
-	"github.com/melkeydev/go-blueprint/cmd/ui/multiInput"
-	"github.com/melkeydev/go-blueprint/cmd/ui/textinput"
-	"github.com/spf13/cobra"
+        tea "github.com/charmbracelet/bubbletea"
+        "github.com/charmbracelet/lipgloss"
+        "github.com/melkeydev/go-blueprint/cmd/program"
+        "github.com/melkeydev/go-blueprint/cmd/steps"
+        "github.com/melkeydev/go-blueprint/cmd/ui/multiInput"
+        "github.com/melkeydev/go-blueprint/cmd/ui/textinput"
+        "github.com/spf13/cobra"
 )
 
 const logo = `
@@ -24,109 +23,130 @@ const logo = `
 | |_) | | |_| |  __/ |_) | |  | | | | | |_ 
 |____/|_|\__,_|\___| .__/|_|  |_|_| |_|\__|
 				   | |                     
-				   |_|                     
+				   |_|    
 
 `
 
 var (
-	logoStyle           = lipgloss.NewStyle().Foreground(lipgloss.Color("#01FAC6")).Bold(true)
-	endingMsgStyle      = lipgloss.NewStyle().PaddingLeft(1).Foreground(lipgloss.Color("170")).Bold(true)
-	allowedProjectTypes = []string{"chi", "gin", "fiber", "gorilla/mux", "httprouter", "standard-library", "echo"}
+        logoStyle           = lipgloss.NewStyle().Foreground(lipgloss.Color("#01FAC6")).Bold(true)
+        endingMsgStyle      = lipgloss.NewStyle().PaddingLeft(1).Foreground(lipgloss.Color("170")).Bold(true)
+        allowedProjectTypes = []string{"chi", "gin", "fiber", "gorilla/mux", "httprouter", "standard-library", "echo"}
 )
 
 func init() {
-	rootCmd.AddCommand(createCmd)
+        rootCmd.AddCommand(createCmd)
 
-	createCmd.Flags().StringP("name", "n", "", "Name of project to create")
-	createCmd.Flags().StringP("framework", "f", "", fmt.Sprintf("Framework to use. Allowed values: %s", strings.Join(allowedProjectTypes, ", ")))
+        createCmd.Flags().StringP("name", "n", "", "Name of project to create")
+        createCmd.Flags().StringP("framework", "f", "", fmt.Sprintf("Framework to use. Allowed values: %s", strings.Join(allowedProjectTypes, ", ")))
 }
 
 // createCmd defines the "create" command for the CLI
 var createCmd = &cobra.Command{
-	Use:   "create",
-	Short: "Create a Go project and don't worry about the structure",
-	Long:  "Go Blueprint is a CLI tool that allows you to focus on the actual Go code, and not the project structure. Perfect for someone new to the Go language",
+        Use:   "create",
+        Short: "Create a Go project and don't worry about the structure",
+        Long:  "Go Blueprint is a CLI tool that allows you to focus on the actual Go code, and not the project structure. Perfect for someone new to the Go language",
 
-	Run: func(cmd *cobra.Command, args []string) {
-		var tprogram *tea.Program
+        Run: func(cmd *cobra.Command, args []string) {
+                var tprogram *tea.Program
+                var errMsg string
 
-		options := steps.Options{
-			ProjectName: &textinput.Output{},
-		}
+                options := steps.Options{
+                        ProjectName: &textinput.Output{},
+                }
 
-		flagName := cmd.Flag("name").Value.String()
-		flagFramework := cmd.Flag("framework").Value.String()
+                flagName := cmd.Flag("name").Value.String()
+                if doesDirectoryExist(flagName) {
+                        errMsg = fmt.Sprintf("Directory '%s' already exists", flagName)
+                        cobra.CheckErr(textinput.CreateErrorValue(errMsg).Error())
+                }
 
-		if flagFramework != "" {
-			isValid := isValidProjectType(flagFramework, allowedProjectTypes)
-			if !isValid {
-				cobra.CheckErr(fmt.Errorf("Project type '%s' is not valid. Valid types are: %s", flagFramework, strings.Join(allowedProjectTypes, ", ")))
-			}
-		}
+                flagFramework := cmd.Flag("framework").Value.String()
+                if flagFramework != "" {
+                        isValid := isValidProjectType(flagFramework, allowedProjectTypes)
+                        if !isValid {
+                                errMsg = fmt.Sprintf("Project type '%s' is not valid. Valid types are: %s", flagFramework, strings.Join(allowedProjectTypes, ", "))
+                                cobra.CheckErr(textinput.CreateErrorValue(errMsg))
+                        }
+                }
 
-		project := &program.Project{
-			FrameworkMap: make(map[string]program.Framework),
-			ProjectName:  flagName,
-			ProjectType:  strings.ReplaceAll(flagFramework, "-", " "),
-		}
+                project := &program.Project{
+                        FrameworkMap: make(map[string]program.Framework),
+                        ProjectName:  flagName,
+                        ProjectType:  strings.ReplaceAll(flagFramework, "-", " "),
+                }
 
-		steps := steps.InitSteps(&options)
-		fmt.Printf("%s\n", logoStyle.Render(logo))
+                steps := steps.InitSteps(&options)
+                fmt.Printf("%s\n", logoStyle.Render(logo))
 
-		if project.ProjectName == "" {
-			tprogram := tea.NewProgram(textinput.InitialTextInputModel(options.ProjectName, "What is the name of your project?", project))
+                if project.ProjectName == "" {
+                        tprogram := tea.NewProgram(textinput.InitialTextInputModel(options.ProjectName, "What is the name of your project?", project))
 
-			if _, err := tprogram.Run(); err != nil {
-				log.Printf("Name of project contains an error: %v", err)
-				cobra.CheckErr(err)
-			}
-			project.ExitCLI(tprogram)
+                        if _, err := tprogram.Run(); err != nil {
+                                errMsg = fmt.Sprintf("Name of project contains an error: %v", err)
+                                cobra.CheckErr(textinput.CreateErrorValue(errMsg).Error())
+                        }
 
-			project.ProjectName = options.ProjectName.Output
-		}
+                        if doesDirectoryExist(options.ProjectName.Output) {
+                                errMsg = fmt.Sprintf("Directory '%s' already exists", options.ProjectName.Output)
+                                cobra.CheckErr(textinput.CreateErrorValue(errMsg).Error())
+                        }
 
-		if project.ProjectType == "" {
-			for _, step := range steps.Steps {
-				s := &multiInput.Selection{}
-				tprogram = tea.NewProgram(multiInput.InitialModelMulti(step.Options, s, step.Headers, project))
-				if _, err := tprogram.Run(); err != nil {
-					cobra.CheckErr(err)
-				}
-				project.ExitCLI(tprogram)
+                        project.ExitCLI(tprogram)
 
-				*step.Field = s.Choice
-			}
+                        project.ProjectName = options.ProjectName.Output
+                }
 
-			project.ProjectType = strings.ToLower(options.ProjectType)
-		}
+                if project.ProjectType == "" {
+                        for _, step := range steps.Steps {
+                                s := &multiInput.Selection{}
+                                tprogram = tea.NewProgram(multiInput.InitialModelMulti(step.Options, s, step.Headers, project))
+                                if _, err := tprogram.Run(); err != nil {
+                                        errMsg = err.Error()
+                                        cobra.CheckErr(textinput.CreateErrorValue(errMsg).Error())
+                                }
+                                project.ExitCLI(tprogram)
 
-		currentWorkingDir, err := os.Getwd()
-		if err != nil {
-			log.Printf("could not get current working directory: %v", err)
-			cobra.CheckErr(err)
-		}
+                                *step.Field = s.Choice
+                        }
 
-		project.AbsolutePath = currentWorkingDir
+                        project.ProjectType = strings.ToLower(options.ProjectType)
+                }
 
-		// This calls the templates
-		err = project.CreateMainFile()
-		if err != nil {
-			log.Printf("Problem creating files for project. %v", err)
-			cobra.CheckErr(err)
-		}
+                currentWorkingDir, err := os.Getwd()
+                if err != nil {
+                        errMsg = fmt.Sprintf("could not get current working directory: %v", err)
+                        cobra.CheckErr(textinput.CreateErrorValue(errMsg).Error())
+                }
 
-		fmt.Println(endingMsgStyle.Render("\nNext steps cd into the newly created project with:"))
-		fmt.Println(endingMsgStyle.Render(fmt.Sprintf("• cd %s\n", project.ProjectName)))
-	},
+                project.AbsolutePath = currentWorkingDir
+
+                // This calls the templates
+                err = project.CreateMainFile()
+                if err != nil {
+                        errMsg = fmt.Sprintf("Problem creating files for project. %v", err)
+                        cobra.CheckErr(textinput.CreateErrorValue(errMsg).Error())
+                }
+
+                fmt.Println(endingMsgStyle.Render("\nNext steps cd into the newly created project with:"))
+                fmt.Println(endingMsgStyle.Render(fmt.Sprintf("• cd %s\n", project.ProjectName)))
+        },
 }
 
-// isValidProjectType checks if the inputted project type matches 
+// isValidProjectType checks if the inputted project type matches
 // the currently supported list of project types
 func isValidProjectType(input string, allowedTypes []string) bool {
-	for _, t := range allowedTypes {
-		if input == t {
-			return true
-		}
-	}
-	return false
+        for _, t := range allowedTypes {
+                if input == t {
+                        return true
+                }
+        }
+        return false
+}
+
+// doesDirectoryExist checks if the directory exists
+func doesDirectoryExist(dir string) bool {
+        if _, err := os.Stat(dir); !os.IsNotExist(err) {
+                return true
+        }
+        return false
 }
--- cmd/ui/textinput/textinput.go
+++ cmd/ui/textinput/textinput.go
@@ -3,97 +3,124 @@
 package textinput
 
 import (
-	"fmt"
+        "errors"
+        "fmt"
 
-	"github.com/charmbracelet/bubbles/textinput"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
-	"github.com/melkeydev/go-blueprint/cmd/program"
+        "github.com/charmbracelet/bubbles/textinput"
+        tea "github.com/charmbracelet/bubbletea"
+        "github.com/charmbracelet/lipgloss"
+        "github.com/melkeydev/go-blueprint/cmd/program"
 )
 
-var titleStyle = lipgloss.NewStyle().Background(lipgloss.Color("#01FAC6")).Foreground(lipgloss.Color("#030303")).Bold(true).Padding(0, 1, 0)
+var (
+        titleStyle = lipgloss.NewStyle().Background(lipgloss.Color("#01FAC6")).Foreground(lipgloss.Color("#030303")).Bold(true).Padding(0, 1, 0)
+        errorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#FF8700")).Bold(true).Padding(0, 0, 0)
+)
 
 type (
-	errMsg error
+        errMsg error
 )
 
 // Output represents the text provided in a textinput step
 type Output struct {
-	Output string
+        Output string
 }
 
 // Output.update updates the value of the Output
 func (o *Output) update(val string) {
-	o.Output = val
+        o.Output = val
 }
 
 // A textnput.model contains the data for the textinput step.
 //
 // It has the required methods that make it a bubbletea.Model
 type model struct {
-	textInput textinput.Model
-	err       error
-	output    *Output
-	header    string
-	exit      *bool
+        textInput textinput.Model
+        err       error
+        output    *Output
+        header    string
+        exit      *bool
 }
 
 // InitialTextInputModel initializes a textinput step
 // with the given data
 func InitialTextInputModel(output *Output, header string, program *program.Project) model {
-	ti := textinput.New()
-	ti.Focus()
-	ti.CharLimit = 156
-	ti.Width = 20
+        ti := textinput.New()
+        ti.Focus()
+        ti.CharLimit = 156
+        ti.Width = 20
 
-	return model{
-		textInput: ti,
-		err:       nil,
-		output:    output,
-		header:    titleStyle.Render(header),
-		exit:      &program.Exit,
-	}
+        return model{
+                textInput: ti,
+                err:       nil,
+                output:    output,
+                header:    titleStyle.Render(header),
+                exit:      &program.Exit,
+        }
+}
+
+// CreateErrorValue creates a textinput step with an error message
+// with the given error message
+func CreateErrorValue(errMsg string) model {
+        ti := textinput.New()
+        ti.Focus()
+        ti.CharLimit = 156
+        ti.Width = 20
+        exit := true
+
+        return model{
+                textInput: ti,
+                err:       errors.New(errorStyle.Render(errMsg)),
+                output:    nil,
+                header:    "",
+                exit:      &exit,
+        }
 }
 
 // Init is called at the beginning of a textinput step
 // and sets the cursor to blink
 func (m model) Init() tea.Cmd {
-	return textinput.Blink
+        return textinput.Blink
 }
 
 // Update is called when "things happen", it checks for the users text input,
 // and for Ctrl+C or Esc to close the program.
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
+        var cmd tea.Cmd
 
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyEnter:
-			if len(m.textInput.Value()) > 1 {
-				m.output.update(m.textInput.Value())
-				return m, tea.Quit
-			}
-		case tea.KeyCtrlC, tea.KeyEsc:
-			*m.exit = true
-			return m, tea.Quit
-		}
+        switch msg := msg.(type) {
+        case tea.KeyMsg:
+                switch msg.Type {
+                case tea.KeyEnter:
+                        if len(m.textInput.Value()) > 1 {
+                                m.output.update(m.textInput.Value())
+                                return m, tea.Quit
+                        }
+                case tea.KeyCtrlC, tea.KeyEsc:
+                        *m.exit = true
+                        return m, tea.Quit
+                }
 
-	// We handle errors just like any other message
-	case errMsg:
-		m.err = msg
-		*m.exit = true
-		return m, nil
-	}
+        // We handle errors just like any other message
+        case errMsg:
+                m.err = msg
+                *m.exit = true
+                return m, nil
+        }
 
-	m.textInput, cmd = m.textInput.Update(msg)
-	return m, cmd
+        m.textInput, cmd = m.textInput.Update(msg)
+        return m, cmd
 }
 
 // View is called to draw the textinput step
 func (m model) View() string {
-	return fmt.Sprintf("%s\n\n%s\n\n",
-		m.header,
-		m.textInput.View(),
-	)
+        return fmt.Sprintf("%s\n\n%s\n\n",
+                m.header,
+                m.textInput.View(),
+        )
+}
+
+// Error returns the error message
+func (m model) Error() string {
+        return m.err.Error()
 }
