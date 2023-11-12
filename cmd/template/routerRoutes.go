43228a467929c860af429a31c613d66cd4f0e748
Mihai Matache <mihai.matache@cyscale.com>
2023-11-12 20:50:31 +0200

    feat: move templates to embedded files (#51)
    * feat: move templates to embedded files
    
    * feat: move gitignore to embed files and updates to make and readme

--- cmd/template/routerRoutes.go
+++ cmd/template/routerRoutes.go
@@ -1,51 +1,22 @@
 package template
 
+import (
+	_ "embed"
+)
+
+//go:embed files/routes/http_router.go.tmpl
+var httpRouterRoutesTemplate []byte
+
 // RouterTemplates contains the methods used for building
 // an app that uses [github.com/julienschmidt/httprouter]
 type RouterTemplates struct{}
 
 func (r RouterTemplates) Main() []byte {
-	return MainTemplate()
+	return mainTemplate
 }
 func (r RouterTemplates) Server() []byte {
-	return MakeHTTPServer()
+	return standardServerTemplate
 }
 func (r RouterTemplates) Routes() []byte {
-	return MakeRouterRoutes()
-}
-
-// MakeRouterRoutes returns a byte slice that represents 
-// the internal/server/routes.go file when using HttpRouter
-func MakeRouterRoutes() []byte {
-	return []byte(`package server
-
-import (
-	"encoding/json"
-	"log"
-	"net/http"
-
-	"github.com/julienschmidt/httprouter"
-)
-
-func (s *Server) RegisterRoutes() http.Handler {
-	r := httprouter.New()
-	r.HandlerFunc(http.MethodGet, "/", s.helloWorldHandler)
-
-	return r
-}
-
-func (s *Server) helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	resp := make(map[string]string)
-	resp["message"] = "Hello World"
-
-	jsonResp, err := json.Marshal(resp)
-	if err != nil {
-		log.Fatalf("error handling JSON marshal. Err: %v", err)
-	}
-
-	w.Write(jsonResp)
-}
-
-
-`)
+	return httpRouterRoutesTemplate
 }
