c059aea1430831cb238fff0940d91934c6aa3f31
Mitchell Berendhuysen <mitchellbhuysen@hotmail.com>
2023-11-15 11:02:46 +0100

    Updated Set function for Framework

--- cmd/flags/frameworks.go
+++ cmd/flags/frameworks.go
@@ -31,11 +31,10 @@ func (f *Framework) Type() string {
 }
 
 func (f *Framework) Set(value string) error {
-	switch value {
-	case Chi.String(), Gin.String(), Fiber.String(), GorillaMux.String(), HttpRouter.String(), StandardLibrary.String(), Echo.String():
+	if AllowedProjectTypes.Contains(value) {
 		*f = Framework(value)
 		return nil
-	default:
-		return fmt.Errorf("Framework to use. Allowed values: %s", strings.Join(AllowedProjectTypes, ", "))
 	}
+
+	return fmt.Errorf("Framework to use. Allowed values: %s", strings.Join(AllowedProjectTypes, ", "))
 }
