c33487fba8fd2fb7a52ce82c09fbbd333f81a2f9
Emil Malmsten <emil_malmsten@hotmail.com>
2023-11-22 15:04:00 +0700

    fix: empty version string

--- cmd/version.go
+++ cmd/version.go
@@ -5,12 +5,23 @@ package cmd
 
 import (
 	"fmt"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
 
 // GoBlueprintVersion is the version of the cli to be overwritten by goreleaser in the CI run with the version of the release in github
 var GoBlueprintVersion string
+
+func getGoBlueprintVersion() string {
+	if len(GoBlueprintVersion) != 0 {
+		return GoBlueprintVersion
+	}
+	if info, ok := debug.ReadBuildInfo(); ok {
+		return info.Main.Version
+	}
+	return "unknown"
+}
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
@@ -19,6 +30,7 @@ var versionCmd = &cobra.Command{
 	Long: `The version command provides information about the application's version.
 Use this command to check the current version of the application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Go Blueprint CLI version %v\n", GoBlueprintVersion)
+		version := getGoBlueprintVersion()
+		fmt.Printf("Go Blueprint CLI version %v\n", version)
 	},
 }
