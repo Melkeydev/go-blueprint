dcabc4c9fe2eb513795a2a5e99707670daaa9df0
ujstor <astipan@gmail.com>
2023-11-24 20:50:27 +0100

    env example templates

--- cmd/template/dbdriver/mongo.go
+++ cmd/template/dbdriver/mongo.go
@@ -12,10 +12,17 @@ var mongoServiceTemplate []byte
 //go:embed files/env/mongo.tmpl
 var mongoEnvTemplate []byte
 
+//go:embed files/env/mongo.example.tmpl
+var mongoEnvExampleTemplate []byte
+
 func (m MongoTemplate) Service() []byte {
 	return mongoServiceTemplate
 }
 
 func (m MongoTemplate) Env() []byte {
 	return mongoEnvTemplate
+}
+
+func (m MongoTemplate) EnvExample() []byte {
+	return mongoEnvExampleTemplate
 }
