c713f8a568bbbe0db561cc80e9c7ba5f73cad626
Mihai Matache <mihai.matache@cyscale.com>
2023-11-25 19:52:26 +0200

    feat: add spinner v4 (#96)
    * wip
    
    * fix: added CLI exit
    
    * fix: release the terminal at the end of the run
    
    * fix: handle error terminal end error
    
    * fix: only show spinner in interactive mode
    
    * ci: remove templates after matrix ends
    
    * fix: remove ucreate main from go routine
    
    * fix: remove the channel
    
    * ffix: wait for spinner to finish
    
    * ci: removes go install from matrix job
    
    * fix: remove duplicate error handling
    
    ---------
    
    Co-authored-by: Mohammad_Alhallaq <mohammad.alhallaq1@gmail.com>
    Co-authored-by: MelkeyDev <53410236+Melkeydev@users.noreply.github.com>

--- cmd/ui/textinput/textinput.go
+++ cmd/ui/textinput/textinput.go
@@ -17,6 +17,7 @@ var (
 	titleStyle = lipgloss.NewStyle().Background(lipgloss.Color("#01FAC6")).Foreground(lipgloss.Color("#030303")).Bold(true).Padding(0, 1, 0)
 	errorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#FF8700")).Bold(true).Padding(0, 0, 0)
 )
+
 type (
 	errMsg error
 )
