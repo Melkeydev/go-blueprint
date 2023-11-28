b0237843ecb47767a170f5f8cb5c5b96af4c8564
Emil Malmsten <emil_malmsten@hotmail.com>
2023-11-28 16:53:27 +0800

    add revision hash and date as fallback version info

--- cmd/version.go
+++ cmd/version.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"fmt"
 	"runtime/debug"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -13,24 +14,59 @@ import (
 // GoBlueprintVersion is the version of the cli to be overwritten by goreleaser in the CI run with the version of the release in github
 var GoBlueprintVersion string
 
+// Go Blueprint needs to be built in a specific way to provide useful version information
+// First we try to get the version from ldflags embedded into GoBlueprintVersion
+// Secondly we try to get the version from from the embedded build info
+// Finally we try to get the version from other embedded VCS info
 func getGoBlueprintVersion() string {
+	noVersionAvailable := "No version info available for this build, run 'go-blueprint help version' for additional info"
+	
 	if len(GoBlueprintVersion) != 0 {
 		return GoBlueprintVersion
 	}
-	if info, ok := debug.ReadBuildInfo(); ok {
-		return info.Main.Version
+
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return noVersionAvailable
 	}
-	return "unknown"
+
+	// If no main version is available, Go defaults it to (devel)
+	if bi.Main.Version != "(devel)" {
+		return bi.Main.Version
+	}
+
+	var vcsRevision string
+	var vcsTime time.Time
+	for _, setting := range bi.Settings {
+		switch setting.Key {
+		case "vcs.revision":
+			vcsRevision = setting.Value
+		case "vcs.time":
+			vcsTime, _ = time.Parse(time.RFC3339, setting.Value)
+		}
+	}
+
+	if vcsRevision != "" {
+		return fmt.Sprintf("%s, (%s)", vcsRevision, vcsTime)
+	}
+
+	return noVersionAvailable
 }
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Display application version information.",
-	Long: `The version command provides information about the application's version.
-Use this command to check the current version of the application.`,
+	Long: `
+The version command provides information about the application's version.
+
+Go Blueprint requires version information to be embedded at compile time.
+For detailed version information, Go Blueprint needs to be built as specified in the installation instructions.
+If Go Blueprint is built within a version control repository, the Go command will
+embed the revision hash if available.
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
 		version := getGoBlueprintVersion()
-		fmt.Printf("Go Blueprint CLI version %v\n", version)
+		fmt.Printf("Go Blueprint CLI version: %v\n", version)
 	},
 }
