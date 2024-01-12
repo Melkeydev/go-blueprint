0c19c552c7ac9e1a9bf475d30a18d17b43bb894b
Brian Barrow <brianbarrow@Brians-MacBook-Pro.local>
2024-01-11 19:30:56 -0700

    add check for git config

--- cmd/utils/utils.go
+++ cmd/utils/utils.go
@@ -86,3 +86,20 @@ func GoTidy(appDir string) error {
 	}
 	return nil
 }
+
+func CheckGitConfig(key string) (bool, error) {
+	cmd := exec.Command("git", "config", "--get", key)
+	if err := cmd.Run(); err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			// The command failed to run.
+			if exitError.ExitCode() == 1 {
+				// The 'git config --get' command returns 1 if the key was not found.
+				return false, nil
+			}
+		}
+		// Some other error occurred.
+		return false, err
+	}
+	// The command ran successfully, so the key is set.
+	return true, nil
+}
