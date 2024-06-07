a8b7390dbac9005a48b04d6eef37c4a1e1accc2e
Kodlak15 <stanlcod15@gmail.com>
2024-06-07 11:16:56 -0700

    flake: remove commented code

--- flake.nix
+++ flake.nix
@@ -18,16 +18,8 @@
         default = let
           module = pkgs.buildGoModule {
             name = "go-blueprint";
-            # version = "0.5.14";
 
             src = ./.;
-
-            # src = pkgs.fetchFromGitHub {
-            #   owner = "Melkeydev";
-            #   repo = "go-blueprint";
-            #   rev = "v${version}";
-            #   hash = "sha256-vQ7LXC70MpqdHxR4JpM5iRQ4mTO0MVtZF94G/f8sR6A=";
-            # };
 
             vendorHash = "sha256-WBzToupC1/O70OYHbKk7S73OEe7XRLAAbY5NoLL7xvw=";
 
