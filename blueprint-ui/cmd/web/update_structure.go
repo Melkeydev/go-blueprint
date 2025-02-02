75c97a2b59f3bf64d76fd4760480c90bb4f8cf57
Ujstor <astipan@gmail.com>
2025-02-02 11:30:25 +0100

    frontend flag impl

---
+++ blueprint-ui/cmd/web/update_structure.go
@@ -0,0 +1,52 @@
+package web
+
+import (
+	"blueprint-ui/cmd/web/components"
+	"net/http"
+)
+
+func contains(slice []string, value string) bool {
+	for _, item := range slice {
+		if item == value {
+			return true
+		}
+	}
+	return false
+}
+
+func UpdateStructureHandler(w http.ResponseWriter, r *http.Request) {
+	// Set HTMX response headers
+	w.Header().Set("HX-Trigger", "updateComplete")
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	advancedOptions, ok := r.Form["advancedOptions"]
+	if !ok {
+		advancedOptions = []string{}
+	}
+
+	advancedFrontend, ok := r.Form["advancedFrontend"]
+	if !ok {
+		advancedFrontend = []string{}
+	}
+
+	options := components.OptionsStruct{
+		ProjectName:      r.FormValue("projectName"),
+		SelectedBackend:  r.FormValue("backend"),
+		SelectedDB:       r.FormValue("database"),
+		SelectGit:        r.FormValue("git"),
+		SelectFrontend:   r.FormValue("frontend"),
+		AdvancedFrontend: advancedFrontend,
+		AdvancedOptions:  advancedOptions,
+	}
+
+	commandStr := components.GetCommandString(options)
+	err = components.FolderStructure(options, commandStr).Render(r.Context(), w)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
