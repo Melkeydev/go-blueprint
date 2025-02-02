5bc3105bf5cadd9f78f5736e587061d54c56fe2d
Ujstor <astipan@gmail.com>
2025-02-02 11:38:33 +0100

    remove bp_ui to sepate branch

--- blueprint-ui/cmd/web/efs.go
+++
@@ -1,6 +0,0 @@
-package web
-
-import "embed"
-
-//go:embed "assets"
-var Files embed.FS
--- blueprint-ui/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./cmd/web/**/*.templ"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
-};
